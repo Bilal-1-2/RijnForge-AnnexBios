aebc7a43582524f6a69c4fa54208b3261af01508
Bilal-1-2 <bilal127g14@gmail.com>
2025-09-10 15:52:00 +0200

    the header is complete

---
+++ assets/includes/header.php
@@ -0,0 +1,40 @@
+
+  <header>
+      <div id="header-top">
+        <div id="header-top-container">
+        <div>
+          <a href="index.html">
+            <img class="logo" src="assets/logo/logo.PNG" alt=""
+          /></a>
+        </div>
+        <div id="header-top-links" class="links">
+          <a href="#">FILM AGENDA</a>
+          <a href="#">ALLE VESTIGINGEN</a>
+          <a href="#">CONTACT</a>
+        </div>
+      </div>
+</div>
+      <div id="header-bottom">
+        <div id="header-bottom-container">
+        <div id="purchase-ticket" class="links">
+          <a href="">KOOP JE TICKETS</a>
+        </div>
+
+        <div class="dropdown">
+          Kies je film
+
+          <div class="dropdown-content">
+            <a href="#">film 1 </a>
+            <a href="#">film 2</a>
+            <a href="#">film 3</a>
+            <a href="#">film 4</a>
+          </div>
+        </div>
+
+        <div id="order-link" class="links">
+          <a href="#">BESTEL TICKETS </a>
+        </div>
+      </div>
+        </div>
+   
+  </header>
