68396bdabd52a3a6b0a8e4ab3c258a9dbac972ce
Reiko-dot <raychako@outlook.com>
2025-09-29 09:56:33 +0200

    bedankt pagina

--- bedankt-pagina.php
+++ bedankt-pagina.php
@@ -17,11 +17,42 @@
     include 'assets/includes/header.php';
     ?>
 
-    <div class="bedankt-pagina-container">
+    <div class="bedankt-title-container">
         <h1>Bedankt voor je bestelling!</h1>
-        <p>Je ontvangt zo snel mogelijk een bevestigingsmail met daarin je tickets.</p>
-        <a href="index.php" class="terug-naar-home-button">Terug naar home</a>
+    </div>
 
+    <div class="bedankt-pagina-container">
+        
+        <div class="poster-section">
+            <img src="assets/films/Jurassic-World_-Fallen-Kingdom.jpg" alt="poster" class="poster-image">  
+        </div>
+
+        <div class="info-section">
+        
+                <h2>Je bestelling is succesvol afgerond.</h2>
+                <p>Hierin vind je alle details van je bestelling.</p>
+                
+                <div class="info-details">
+                    <div class="film-title">Film: Jurassic World: Fallen Kingdom</div>
+                    <div class="datum">Datum: 29-09-25</div>
+                    <div class="tijdsduur">Tijd: 2u 9 min</div>
+                    <div class="ticket-aantal">tickets: 2</div>
+                    <div class="stoelen">Stoelen: Rij 5 zitplaatsen 7 tot 8</div>
+                    <div class="bedrag">Bedrag: €14,00</div>
+                </div>
+
+
+        
+
+        </div>
+                
+            
+
+        
+        
+       
+
+    </div>
  
         <?php
         include 'assets/includes/footer.php';
